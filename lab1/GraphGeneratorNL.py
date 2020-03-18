24d7c6c1b5b7b5743a504c71c362cd5aa526d6d3
Anna Malik <anka98@op.pl>
2020-03-18 20:21:42 +0100

    init and generators

---
+++ lab1/GraphGeneratorNL.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plot
+import networkx 
+from itertools import combinations
+from random import sample
+
+def GraphGeneratorNL(n, l):
+    V = set([v for v in range(n)])
+    E = set()
+
+    comb = combinations(V,2)
+    random_comb = sample(list(comb), l)
+    for c in random_comb:
+        E.add(c)
+
+    graph = networkx.Graph()
+    graph.add_nodes_from(V)
+    graph.add_edges_from(E)
+
+    return graph
+
+
+
+#test
+n = 10
+l = 7
+G = GraphGeneratorNL(n, l)
+
+pos = networkx.spring_layout(G)
+networkx.draw_networkx(G, pos)
+plot.show()
---
+++ lab1/GraphGeneratorNP.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plot
+import networkx
+from itertools import combinations
+from random import random
+
+
+def GraphGeneratorNP(n, p):
+    V = set([v for v in range(n)])
+    E = set()
+
+    for c in combinations(V, 2):
+        tmp = random()
+        if tmp < p:
+            E.add(c)
+
+    graph = networkx.Graph()
+    graph.add_nodes_from(V)
+    graph.add_edges_from(E)
+
+    return graph
+
+
+
+#test
+n = 6
+p = 0.8
+G = GraphGeneratorNP(n, p)
+pos = networkx.spring_layout(G)
+networkx.draw_networkx(G, pos)
+plot.show()
