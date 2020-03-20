2a3dae1c6713a4e6b64575bb25834e1ce11ce0e9
Anna Malik <anka98@op.pl>
2020-03-20 18:57:15 +0100

    me 0:1 pyplot, sry

--- lab1/drawGraph.py
+++ lab1/drawGraph.py
@@ -8,14 +8,27 @@ import networkx
 def draw(graph):
     x = []
     y = []
+    nodes_from = list()
+    nodes_to = list()
     nodes = list(graph.nodes)
+    print(nodes)
     for n in range(len(nodes)):
-        angle = random.uniform(0,6)*(math.pi*2)
+        angle = random.uniform(0,1)*(math.pi*2)
         x.append(math.cos(angle))
         y.append(math.sin(angle))
     plt.scatter(x,y)
     plt.axes().set_aspect('equal', 'datalim')
+    # for node in graph.nodes():
+    #     plt.plot(node)
+
+    for edge in graph.edges():
+        nodes_from.append(edge[0])
+        nodes_to.append(edge[1])
+        plt.plot(edge)
+
+    #plt.plot(nodes_from, nodes_to)
     plt.show()
+
 
 
 n = 10
