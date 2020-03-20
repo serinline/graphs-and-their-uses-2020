7e0491781eee9b8b66e83ad706b1a5948f11cded
Anna Malik <anka98@op.pl>
2020-03-20 20:26:26 +0100

    adjacency list //bug

--- lab1/ChangeRepresentation.py
+++ lab1/ChangeRepresentation.py
@@ -1,12 +1,14 @@
 import numpy
+from GraphGeneratorNL import GraphGeneratorNL as gr
 
+#here is a bug, index out of range but idk why ><
+def adjacency_list(graph):
+    adj_list = list()
+    for edge in graph.edges():
+        adj_list[edge[0]].append(edge[1])
+        
 
-def changeR(g):
-
-    #is list?
-    if isinstance(g, (list, tuple)):
-
-    #is symmetric?
-    else if (g.transpose() == g).all():
-
-    #else incidence matrix
+n = 10
+l = 7
+G = gr(n, l)
+print(adjacency_list(G))
