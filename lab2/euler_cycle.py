a181832fe7d3c7125b923012469f6959b9557558
Anna Malik <anka98@op.pl>
2020-03-24 17:07:39 +0100

    euler cycle works but sometimes starts from bad node

--- lab2/euler_cycle.py
+++ lab2/euler_cycle.py
@@ -1,10 +1,14 @@
+import networkx
+import matplotlib.pyplot as plot
+
 from lab1.graph_generator_nl import graph_generator_nl
 from lab1.representations import adjacency_matrix
 
 
-def euler_path(graph):
-    path = []
+def euler_cycle(graph):
+    cycle = []
     adj_matrix = adjacency_matrix(graph)
+    plot
 
     def dfs_euler(v):
         for u in range(len(graph.edges)):
@@ -12,15 +16,18 @@ def euler_path(graph):
                 adj_matrix[v][u] -= 1
                 adj_matrix[u][v] -= 1
                 dfs_euler(u)
-        path.append(v)
+        cycle.append(v)
 
     nodes = list(graph.nodes)
     dfs_euler(nodes[0])
-    return path
+    return cycle
 
 
 n = 10
 l = 7
 G = graph_generator_nl(n, l)
-eul = euler_path(G)
+eul = euler_cycle(G)
+pos = networkx.circular_layout(G)
+networkx.draw_networkx(G, pos)
+plot.show()
 print(eul)
