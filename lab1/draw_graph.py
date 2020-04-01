426d5b1150c917b88bc63ccc6df502142e786b39
Anna Malik <anka98@op.pl>
2020-04-01 16:46:54 +0200

    very little cleaning

--- lab1/draw_graph.py
+++ lab1/draw_graph.py
@@ -6,20 +6,17 @@ from lab1.representations import adjacency_list
 
 def draw(graph):
     nodes = list(graph.nodes)
-    fig = plt.figure(figsize=(5, 5))
+    plt.figure(figsize=(5, 5))
     num_nodes = len(nodes)
     angle = np.linspace(0, 2*np.pi, num_nodes)
     cx, cy = (10, 10)
     radius = 5.0
-    x_node = cx + radius*np.cos(angle)
-    y_node = cy + radius*np.sin(angle)
-
-    plt.scatter(x_node, y_node, c = 'blue', s=50)
-
     graph_list = adjacency_list(graph)
+
     for i in range(num_nodes):
         x_node = cx + radius*np.cos(angle)
         y_node = cy + radius*np.sin(angle)
+        plt.scatter(x_node, y_node, c='blue', s=50)
         for node in graph_list[i]:
             next_angle = angle*node
             x_next_node = cx + radius*np.cos(next_angle)
