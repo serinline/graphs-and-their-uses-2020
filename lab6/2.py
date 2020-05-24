2888428b9ce35db0f350cb6e6d2963d6d00f55f3
stelega <szymon.telega98@gmail.com>
2020-05-24 20:30:16 +0200

    lab6.2

--- lab6/2.py
+++ lab6/2.py
@@ -1,9 +1,6 @@
 import os
 from graphlib.Graph import Graph
 from graphlib.algorithms.Algorithms import Algorithms
-from graphlib.Edge import Edge
-from graphlib.Node import Node
-from random import randrange
 
 graph = Graph.create_from_file(os.path.dirname(__file__) + "/files/travelling_salesman.json", "coordinates")
 
